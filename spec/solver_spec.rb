8769ce77ea147ddcc3c395af63a8f968200a4a8e
Anas Abubakar <anasabubakar2015@gmail.com>
2022-10-07 09:32:13 +0100

    Add unit test for the reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,11 +6,16 @@ describe Solver do
 
   it 'should return 120 when given 5' do
     solver = Solver.new
-    expect(@solver.factorial(5)).to eq(120)
+    expect(solver.factorial(5)).to eq(120)
   end
 
   it 'should return error when given -1' do
     solver = Solver.new
     expect { solver.factorial(-1) }.to raise_error(Error)
+  end
+
+  it 'should return olleh when given hello' do
+    solver = Solver.new
+    expect(solver.reverse('hello')).to eq('olleh')
   end
 end
